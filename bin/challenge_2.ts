dd571fa7187022fa46c21c910579f79d6381a9d2
Chris Hsin <chris.hsin@beseye.com>
2021-08-05 17:54:40 +0800

    AWS CDK challenge 2.

--- bin/challenge_2.ts
+++ bin/challenge_2.ts
@@ -1,10 +1,38 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
-import { Challenge2Stack } from '../lib/challenge_2-stack';
+import { StandardService } from '../lib/challenge_2-stack';
+import * as ecs from '@aws-cdk/aws-ecs';
+import * as ec2 from '@aws-cdk/aws-ec2';
 
 const app = new cdk.App();
-new Challenge2Stack(app, 'Challenge2Stack', {
+
+const env = { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION };
+
+const stack = new cdk.Stack(app, 'Challenge2Stack', { env })
+
+const nyancatTask = new ecs.FargateTaskDefinition(stack, 'NyanCatTask', {
+  cpu: 256,
+  memoryLimitMiB: 512,
+})
+
+nyancatTask.addContainer('nyancat', {
+  image: ecs.ContainerImage.fromRegistry('public.ecr.aws/pahudnet/nyancat-docker-image:latest'),
+  portMappings: [ { containerPort: 80 }]
+})
+
+// const vpc = ec2.Vpc.fromLookup(stack, 'Vpc', { vpcId: 'vpc-0a34863cb7953f801' })
+new StandardService(stack, 'DemoService', {
+  tasks: [
+    {
+      task: nyancatTask,
+      external: { port: 80 }
+    }
+  ],
+//  vpc: vpc,
+});
+
+// new Challenge2Stack(app, 'Challenge2Stack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
@@ -18,4 +46,4 @@ new Challenge2Stack(app, 'Challenge2Stack', {
   // env: { account: '123456789012', region: 'us-east-1' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
+// });
--- lib/challenge_2-stack.ts
+++ lib/challenge_2-stack.ts
@@ -1,9 +1,21 @@
 import * as cdk from '@aws-cdk/core';
+import * as ecs from '@aws-cdk/aws-ecs';
+import { DualAlbFargateService, FargateTaskProps } from 'cdk-fargate-patterns';
+import * as ec2 from '@aws-cdk/aws-ec2';
 
-export class Challenge2Stack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
 
-    // The code that defines your stack goes here
+export interface StandardServiceProps {
+  readonly vpc?: ec2.IVpc;
+  readonly tasks: FargateTaskProps[];
+}
+
+export class StandardService extends cdk.Construct {
+  constructor(scope: cdk.Construct, id: string, props: StandardServiceProps) {
+    super(scope, id)
+
+    new DualAlbFargateService(this, 'Service', {
+      tasks: props.tasks,
+      vpc: props.vpc,
+    })
   }
 }
